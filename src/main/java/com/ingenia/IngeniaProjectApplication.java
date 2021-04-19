10a4773d63f0e00cbe9d4ac35a8f5468cb65b020
Jose Antonio <j2a2mari@gmail.com>
2021-04-20 00:03:13 +0200

    base project

---
+++ src/main/java/com/ingenia/IngeniaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ingenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngeniaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngeniaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingenia/IngeniaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngeniaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
