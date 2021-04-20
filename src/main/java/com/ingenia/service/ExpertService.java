30ccb56d514644ce4811ec1a9cf2861963bc7c4d
Jose Antonio <j2a2mari@gmail.com>
2021-04-20 14:54:41 +0200

    add services

--- src/main/java/com/ingenia/service/ExpertService.java
+++ src/main/java/com/ingenia/service/ExpertService.java
@@ -1,4 +1,28 @@
 package com.ingenia.service;
 
+import com.ingenia.model.Expert;
+
+import java.util.List;
+
 public interface ExpertService {
+
+    // consultas y filtros
+
+    public List<Expert> getAllExperts();
+
+    public Expert getExpert(Long id);
+
+    public List<Expert> filterByNameContains(String name);
+
+    // crear
+
+    public Expert createExpert(Expert expert);
+
+    // actualizar
+
+    public Expert updateExpert(Long id, Expert expert);
+
+    // eliminar
+
+    public boolean deleteExpert(Long id);
 }
--- src/main/java/com/ingenia/service/impl/ExpertServiceImpl.java
+++ src/main/java/com/ingenia/service/impl/ExpertServiceImpl.java
@@ -1,7 +1,48 @@
 package com.ingenia.service.impl;
 
+import com.ingenia.dao.ExpertDAO;
+import com.ingenia.model.Expert;
+import com.ingenia.service.ExpertService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ExpertServiceImpl {
+public class ExpertServiceImpl implements ExpertService {
+
+    private ExpertDAO expertDAO;
+
+    public ExpertServiceImpl(ExpertDAO expertDAO) {
+        this.expertDAO = expertDAO;
+    }
+
+    @Override
+    public List<Expert> getAllExperts() {
+        return expertDAO.getAllExperts();
+    }
+
+    @Override
+    public Expert getExpert(Long id) {
+        return expertDAO.getExpert(id);
+    }
+
+    @Override
+    public List<Expert> filterByNameContains(String name) {
+        return expertDAO.filterByNameContains(name);
+    }
+
+    @Override
+    public Expert createExpert(Expert expert) {
+        return expertDAO.createExpert(expert);
+    }
+
+    @Override
+    public Expert updateExpert(Long id, Expert expert) {
+        return expertDAO.updateExpert(id, expert);
+    }
+
+    @Override
+    public boolean deleteExpert(Long id) {
+        return expertDAO.deleteExpert(id);
+    }
 }
