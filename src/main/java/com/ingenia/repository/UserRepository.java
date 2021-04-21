031c70d8045e321f6729eabfdc4325bf59d06c1c
Jose Antonio <j2a2mari@gmail.com>
2021-04-22 00:18:16 +0200

    add user repository

--- src/main/java/com/ingenia/repository/UserRepository.java
+++ src/main/java/com/ingenia/repository/UserRepository.java
@@ -4,6 +4,15 @@ import com.ingenia.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String email);
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String email);
+    Boolean existsByEmail(String email);
+
 }
