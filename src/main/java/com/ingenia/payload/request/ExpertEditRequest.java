454d93097d402c909160726ea642b946e6e4ee89
Jose Antonio <j2a2mari@gmail.com>
2021-04-29 02:59:15 +0200

    add more atribbutes in expert edit request

--- src/main/java/com/ingenia/payload/request/ExpertEditRequest.java
+++ src/main/java/com/ingenia/payload/request/ExpertEditRequest.java
@@ -1,5 +1,9 @@
 package com.ingenia.payload.request;
 
+import com.ingenia.model.State;
+
+import java.util.List;
+
 public class ExpertEditRequest {
 
     private String nombre;
@@ -10,7 +14,19 @@ public class ExpertEditRequest {
 
     private String contacto_email;
 
-    private int[] ids_etiquetas;
+    private List<Long> ids_etiquetas;
+
+    private String direccion;
+
+    private String contacto_linkedin;
+
+    private String disponibilidad;
+
+    private State estado;
+
+    private Integer puntuacion;
+
+    private String observaciones;
 
     public ExpertEditRequest() {
     }
@@ -47,11 +63,59 @@ public class ExpertEditRequest {
         this.contacto_email = contacto_email;
     }
 
-    public int[] getIds_etiquetas() {
+    public List<Long> getIds_etiquetas() {
         return ids_etiquetas;
     }
 
-    public void setIds_etiquetas(int[] ids_etiquetas) {
+    public void setIds_etiquetas(List<Long> ids_etiquetas) {
         this.ids_etiquetas = ids_etiquetas;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getContacto_linkedin() {
+        return contacto_linkedin;
+    }
+
+    public void setContacto_linkedin(String contacto_linkedin) {
+        this.contacto_linkedin = contacto_linkedin;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public State getEstado() {
+        return estado;
+    }
+
+    public void setEstado(State estado) {
+        this.estado = estado;
+    }
+
+    public Integer getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(Integer puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
     }
 }
